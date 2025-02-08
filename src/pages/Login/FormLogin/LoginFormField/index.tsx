15117b383c3c389e1803e2d925d034fd342a4fc4
Lucas Mendonça <lms.devop@gmail.com>
2025-02-07 22:03:48 -0300

    Incorpora a API no front

--- src/pages/Login/FormLogin/LoginFormField/index.tsx
+++ src/pages/Login/FormLogin/LoginFormField/index.tsx
@@ -4,13 +4,20 @@ interface LoginFormFieldProps {
     label: string;
     placeholder: string;
     type: string;
+    value: string;
+    onTyping: (value: string) => void;
 }
 
-const LoginFormField: React.FC<LoginFormFieldProps> = ({ label, placeholder, type }) => {
+const LoginFormField: React.FC<LoginFormFieldProps> = ({ label, placeholder, type, value, onTyping }) => {
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    onTyping(e.target.value);
+  };
+
   return (
     <div className="login-form-field">
       <label>{label}</label>
-      <input type={type} placeholder={placeholder} />
+      <input value={value} onChange={handleChange} type={type} placeholder={placeholder} />
     </div>
   );
 };
--- src/pages/Login/FormLogin/index.tsx
+++ src/pages/Login/FormLogin/index.tsx
@@ -1,15 +1,57 @@
+import { useState } from 'react';
 import './FormLogin.css';
 import LoginButton from './LoginButton';
 import LoginFormField from './LoginFormField';
 
 const FormLogin = () => {
+
+  const [user, setUser] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onRegister = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    console.log('Usuário:', user, 'Senha:', password)
+    fetch(`/api/users/?login=${user}&password=${password}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(response => response.json())
+      .then(data => {
+        if (data) {
+          console.log('Login success');
+          // Handle login success
+        } else {
+          console.log('Login failed');
+          // Handle login failure
+        }
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+  }
+
   return (
     <section className="login-form">
-      <form>
+      <form onSubmit={onRegister}>
         <h1>Bem-vindo(a)</h1>
 
-        <LoginFormField label="Usuário" placeholder="Digite seu usuário" type="text" />
-        <LoginFormField label="Senha" placeholder="Digite sua senha" type="password" />
+        <LoginFormField 
+          value={user} 
+          label="Usuário" 
+          placeholder="Digite seu usuário" 
+          type="text"  
+          onTyping={value => setUser(value)}
+          />
+        <LoginFormField 
+          value={password}
+          label="Senha" 
+          placeholder="Digite sua senha" 
+          type="password" 
+          onTyping={value => setPassword(value)}
+          
+          />
         <LoginButton />
       </form>
     </section>
